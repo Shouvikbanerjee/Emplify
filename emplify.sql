7086b17463d2ab166e89133db43315a828d19656
shouvik banerjee <banerjeeshouvik29@gmail.com>
2026-03-29 10:06:29 +0530

    555

--- emplify.sql
+++ emplify.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 10, 2025 at 04:05 PM
+-- Generation Time: Mar 29, 2026 at 05:38 AM
 -- Server version: 10.4.32-MariaDB
--- PHP Version: 8.0.30
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -62,36 +62,18 @@ CREATE TABLE `attendance` (
 --
 
 INSERT INTO `attendance` (`att_id`, `emp_id`, `date`, `check_in`, `check_out`, `status`) VALUES
-(1, 2, '2025-04-30', '17:14:43', NULL, 'Present'),
-(2, 2, '2025-05-03', '06:07:06', NULL, 'Present'),
-(3, 4, '2025-05-04', '04:56:13', NULL, 'Present'),
-(4, 2, '2025-05-10', '07:20:03', NULL, 'Present'),
-(5, 6, '0000-00-00', '09:28:58', '10:12:58', 'Present'),
-(6, 6, '0000-00-00', '09:32:26', '09:33:49', 'Present'),
-(7, 6, '0000-00-00', '09:34:41', '09:34:43', 'Present'),
-(8, 6, '0000-00-00', '09:46:36', '10:12:11', 'Present'),
-(9, 6, '0000-00-00', '09:47:28', '09:47:30', 'Present'),
-(10, 6, '0000-00-00', '10:18:04', '10:21:04', 'Present'),
-(11, 6, '0000-00-00', '10:19:33', '10:20:39', 'Present'),
-(12, 6, '0000-00-00', '10:21:06', '10:21:10', 'Present'),
-(13, 6, '0000-00-00', '10:23:14', '10:23:16', 'Present'),
-(14, 6, '0000-00-00', '10:32:36', '20:53:58', 'Present'),
-(15, 6, '0000-00-00', '10:33:01', '07:17:04', 'Present'),
-(16, 6, '0000-00-00', '10:43:25', '07:16:47', 'Present'),
-(17, 6, '0000-00-00', '14:02:31', '14:02:33', 'Present'),
-(18, 6, '2025-06-03', '17:37:26', '17:37:34', 'Present'),
-(19, 6, '2025-06-04', '07:16:22', '21:37:53', 'Present'),
-(20, 6, '2025-06-04', '07:16:30', '21:31:58', 'Present'),
-(21, 6, '2025-06-04', '08:18:06', '21:31:31', 'Present'),
-(22, 6, '2025-06-04', '08:18:50', '20:54:02', 'Present'),
-(23, 6, '2025-06-09', '21:38:00', '21:38:08', 'Present'),
-(24, 6, '2025-06-09', '21:38:13', '21:38:19', 'Present'),
-(25, 6, '2025-06-09', '21:38:47', '21:38:54', 'Present'),
-(26, 7, '2025-06-09', '21:39:58', '21:40:01', 'Present'),
-(27, 6, '2025-06-09', '21:42:41', '21:42:43', 'Present'),
-(28, 7, '2025-06-09', '21:47:26', NULL, 'Present'),
-(29, 6, '2025-06-10', '10:58:19', '11:01:24', 'Present'),
-(30, 6, '2025-06-10', '19:21:49', '19:22:11', 'Present');
+(39, 6, '2026-03-26', '10:46:50', '10:46:52', 'Present'),
+(40, 6, '2026-03-26', '10:46:54', '10:46:57', 'Present'),
+(41, 6, '2026-03-26', '10:47:07', '10:47:09', 'Present'),
+(42, 6, '2026-03-27', '09:06:59', '09:07:00', 'Present'),
+(43, 6, '2026-03-27', '09:07:02', '09:07:04', 'Present'),
+(44, 6, '2026-03-27', '09:07:05', '09:07:07', 'Present'),
+(45, 7, '2026-03-27', '12:20:23', '12:20:37', 'Present'),
+(46, 6, '2026-03-28', '09:39:09', '10:41:19', 'Present'),
+(47, 6, '2026-03-28', '19:54:05', '19:58:38', 'Present'),
+(48, 7, '2026-03-28', '20:01:01', '20:01:03', 'Present'),
+(49, 7, '2026-03-28', '20:01:04', '20:01:05', 'Present'),
+(50, 7, '2026-03-28', '20:01:07', '20:01:09', 'Present');
 
 -- --------------------------------------------------------
 
@@ -155,20 +137,22 @@ CREATE TABLE `employee` (
   `dob` date DEFAULT NULL,
   `address` text DEFAULT NULL,
   `dep_id` int(11) DEFAULT NULL,
-  `desg_id` int(11) DEFAULT NULL
+  `desg_id` int(11) DEFAULT NULL,
+  `image` text NOT NULL,
+  `leave_balance` int(11) NOT NULL,
+  `last_leave_reset` varchar(7) NOT NULL,
+  `status` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `employee`
 --
 
-INSERT INTO `employee` (`emp_id`, `name`, `email`, `phone`, `gender`, `dob`, `address`, `dep_id`, `desg_id`) VALUES
-(2, 'Rana Garai', 'ranagarai820@gmail.com', '9883169583', 'Male', '0000-00-00', 'Melatala,po: Shyam Nabagram;ps-Monteswar', 4, 5),
-(3, 'Pratap Garai', 'pratap@123gmail.com', '6235452036', 'Male', '2005-04-18', 'Melatala,po: Shyam Nabagram;ps-Monteswar', 4, 5),
-(4, 'Md Shakil', 'shakil6294@gmail.com', '06294030194', 'Male', '2005-03-28', 'Ghatshila', 1, 1),
-(5, 'Bikas Sen', 'bikas1234@gmail.com', '5698754525', 'Male', '0000-00-00', 'xyz', 2, 1),
-(6, 'Arka Dutta', 'arkadutta333@gmail.com', '12345678900', 'Male', '2003-05-18', 'Bankura More', 9, 2),
-(7, 'Soumyadip Hazra', 'soumyadip@123gmail.com', '123456789', 'Male', '2003-02-06', 'xyz', 9, 2);
+INSERT INTO `employee` (`emp_id`, `name`, `email`, `phone`, `gender`, `dob`, `address`, `dep_id`, `desg_id`, `image`, `leave_balance`, `last_leave_reset`, `status`) VALUES
+(2, 'Rana Garai', 'ranagarai820@gmail.com', '9883169583', 'Male', '0000-00-00', 'Melatala,po: Shyam Nabagram;ps-Monteswar', 4, 5, '', 0, '', 1),
+(5, 'Bikas Sen', 'bikas1234@gmail.com', '5698754525', 'Male', '0000-00-00', 'xyz11', 2, 1, '', 0, '', 1),
+(6, 'Arka Dutta', 'arkadutta333@gmail.com', '12345678900', 'Male', '2003-05-18', 'Bankura More', 9, 2, '1774596100_IMG_20231105_103916 copy.jpg', 0, '2026-03', 1),
+(7, 'Soumyadip Hazra', 'soumyadip@123gmail.com', '123456789', 'Male', '2003-02-06', 'Katwa', 9, 2, '', 5, '2026-03', 1);
 
 -- --------------------------------------------------------
 
@@ -216,7 +200,8 @@ CREATE TABLE `leave_management` (
 --
 
 INSERT INTO `leave_management` (`leave_id`, `emp_id`, `leave_type`, `start_date`, `end_date`, `reason`, `status`) VALUES
-(5, 6, 'Casual', '2025-06-11', '2025-06-12', 'casul', 'Approved');
+(13, 6, 'Casual', '2026-03-27', '2026-03-29', 'ds', 'Approved'),
+(14, 6, 'Casual', '2026-03-27', '2026-03-28', 'ee', 'Approved');
 
 -- --------------------------------------------------------
 
@@ -261,8 +246,11 @@ CREATE TABLE `reports` (
 --
 
 INSERT INTO `reports` (`report_id`, `emp_id`, `title`, `description`, `is_site_visit`, `created_at`, `task_id`) VALUES
-(4, 6, 'project1 - Report', '                            Task Description: ddddddddddddddddddddddddddaaaaaaaaaaaaa                        ', 0, '2025-05-31 11:57:12', 25),
-(5, 6, 'project2 - Report', '                           \\r\\n                        kkkkkkkkkkkkkkkk', 0, '2025-05-31 12:18:19', 26);
+(6, 6, 'Hello World - Report', 'wwwwwwwwwwwwwwwwwwwwwwwwwwww', 0, '2026-03-27 03:50:45', 27),
+(8, 6, 'Hello World - Report', 'aaaaaaaaaaaaaaaaaaaa', 0, '2026-03-27 03:59:29', 27),
+(9, 6, 'hello Google  - Report', 'progress', 0, '2026-03-27 04:05:02', 28),
+(10, 6, 'hello Google  - Report', 'Completed', 0, '2026-03-27 04:05:27', 28),
+(11, 6, 'Hello World - Report', 'com', 0, '2026-03-27 07:22:08', 27);
 
 -- --------------------------------------------------------
 
@@ -282,8 +270,11 @@ CREATE TABLE `report_images` (
 --
 
 INSERT INTO `report_images` (`image_id`, `report_id`, `image_path`, `uploaded_at`) VALUES
-(4, 4, 'uploads/reports/683aee980f714_Untitled1.png', '2025-05-31 11:57:12'),
-(5, 5, 'uploads/reports/683af38b51f0c_arya.png', '2025-05-31 12:18:19');
+(6, 6, 'uploads/reports/69c5fe9579b1d_686a91063b5a1_my photo.jpg', '2026-03-27 03:50:45'),
+(8, 8, 'uploads/reports/69c600a1a995d_685a3ef73e3c8_Screenshot (4).png', '2026-03-27 03:59:29'),
+(9, 9, 'uploads/reports/69c601ee004b5_Screenshot (5).png', '2026-03-27 04:05:02'),
+(10, 10, 'uploads/reports/69c60207aa0c6_Screenshot (1).png', '2026-03-27 04:05:27'),
+(11, 11, 'uploads/reports/69c63020ea34e_IMG_20231105_103916 copy.jpg', '2026-03-27 07:22:08');
 
 -- --------------------------------------------------------
 
@@ -327,8 +318,8 @@ CREATE TABLE `task_assigned` (
 --
 
 INSERT INTO `task_assigned` (`task_id`, `emp_id`, `title`, `description`, `assign_date`, `due_date`, `status`, `report`, `file_upload`, `report_submitted_on`, `is_site_visit`, `location`, `feedback`) VALUES
-(25, 6, 'project1', 'dddddddddgSDgSdg', '2025-05-31', '2025-05-31', 'Completed', NULL, NULL, '2025-06-04 11:49:21', 1, 'katwa', 'good'),
-(26, 6, 'project2', 'gfhshsfh', '2025-05-31', '2025-05-31', 'Completed', NULL, NULL, '2025-05-31 05:17:46', 1, 'burdwan', 'fsegf');
+(27, 6, 'Hello World', 'fsdggggggggggggggg', '2026-03-27', '2026-03-27', 'Completed', NULL, NULL, '2026-03-27 09:29:12', 1, 'burdwan', 'ddddddd'),
+(28, 6, 'hello Google ', 'hihstkhertkedshekgeisy ', '2026-03-27', '2026-03-27', 'Completed', NULL, NULL, '2026-03-27 09:35:17', 1, 'katwa', 'Completed');
 
 --
 -- Indexes for dumped tables
@@ -436,7 +427,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `attendance`
 --
 ALTER TABLE `attendance`
-  MODIFY `att_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
+  MODIFY `att_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=51;
 
 --
 -- AUTO_INCREMENT for table `department`
@@ -454,7 +445,7 @@ ALTER TABLE `designation`
 -- AUTO_INCREMENT for table `employee`
 --
 ALTER TABLE `employee`
-  MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+  MODIFY `emp_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT for table `feedback`
@@ -466,25 +457,25 @@ ALTER TABLE `feedback`
 -- AUTO_INCREMENT for table `leave_management`
 --
 ALTER TABLE `leave_management`
-  MODIFY `leave_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `leave_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- AUTO_INCREMENT for table `login`
 --
 ALTER TABLE `login`
-  MODIFY `login_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `login_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `reports`
 --
 ALTER TABLE `reports`
-  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `report_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `report_images`
 --
 ALTER TABLE `report_images`
-  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `image_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=12;
 
 --
 -- AUTO_INCREMENT for table `site_visit_tracking`
@@ -496,7 +487,7 @@ ALTER TABLE `site_visit_tracking`
 -- AUTO_INCREMENT for table `task_assigned`
 --
 ALTER TABLE `task_assigned`
-  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `task_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=29;
 
 --
 -- Constraints for dumped tables
